dacf892b868b475b51dd5f8f8ac15ca4d318eb57
Mission Marcus <missionmarcus@gmail.com>
2021-02-15 20:11:52 -0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
-
+import Input from './components/Input/Input'
 function App() {
   return (
     <div className="App">
-      
+      <div className='headline-title'>
+        Last Five
+      </div>
+      <Input></Input>
     </div>
   );
 }
