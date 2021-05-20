6e62aa5379a1d86de0ed63e413fddbc82c10a34b
Mission Marcus <missionmarcus@gmail.com>
2021-05-20 15:33:01 -0500

    added logic now

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = (props) => {
                     <MostPlayed mostPlayed={lookupSummoner.mostPlayed}></MostPlayed>
                     <Winrate winRate={lookupSummoner.winRate}></Winrate>
                     <AverageDamage averageDamage={lookupSummoner.averageDamage} averageDpm={lookupSummoner.averageDpm}></AverageDamage>
-                    <Grid damageOverGames={[50000, 30000, 20000, 10000, 60000]}></Grid>
+                    <Grid goldOverGames={[50000, 30000, 20000, 10000, 60000]}></Grid>
                 </div>
         </div>
     )
--- src/components/Grid/grid.jsx
+++ src/components/Grid/grid.jsx
@@ -2,28 +2,36 @@ import './grid.css'
 import React, { useEffect, useState } from 'react'
 
 
-const findGreatest = (damageOverGames) => {
+const findGreatest = (goldOverGames) => {
         
     let maximum = Number.MIN_VALUE
-    for (var i in damageOverGames) {
-        if (damageOverGames[i] > maximum) {
-            maximum = damageOverGames[i]
+    for (var i in goldOverGames) {
+        if (goldOverGames[i] > maximum) {
+            maximum = goldOverGames[i]
         }
     }
     return maximum
 }
 
 
+const populateGame = (grid, setGrid, number, goldArray, axisArray) => {
+
+
+}
+
+
 const Grid = (props) => {
 
-    const { damageOverGames } = props
+    const { goldOverGames } = props
 
     const [axisArray, setAxisArray] = useState([])
-
+    const [isLoading, setLoading] = useState(false)
+    const [grid, setGrid] = useState(undefined)
+    const [gameNumber, setGameNumber] = useState(-1)
 
     useEffect(() => {
 
-        const greatest = findGreatest(damageOverGames)
+        const greatest = findGreatest(goldOverGames)
         let populatedArray = []
 
         for (let i = 0; i < 11; i++) {
@@ -31,39 +39,87 @@ const Grid = (props) => {
         }
         
         setAxisArray(populatedArray)
-    }, [damageOverGames])
 
-    
+        let gridSet = []
 
+        for (let j = 0; j < 11; j++) {
+            gridSet[j] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        }
+
+        setGrid(gridSet)
+    }, [])
+
+    useEffect(() => {
+
+            if (!isLoading && grid && gameNumber <= 5) {
+            
+
+                let count = 0
+
+                let goldAmount = goldOverGames[gameNumber]
+            
+                let newGridSet = grid
+
+                while (count <= 10) {
+                    if ((goldAmount >= axisArray[count] * 1000) && (count <= 10)) {
+                        newGridSet[10-count][(gameNumber + 1) * 2 - 1] = 1
+                        setGrid(newGridSet)
+                    }
+                    count = count + 1;
+                }
+            
+                setGrid(newGridSet)
+                setGameNumber(gameNumber + 1)
+        
+            }
+
+        
+        
+    }, [grid, gameNumber, isLoading])
+
+    if (gameNumber === -1) {
+        console.log(gameNumber)
+        setGameNumber(0)
+    }
 
     return (
-        <div className='grid-container'>
-        <div className='grid-y-axis'>
-            {axisArray.map((i) => 
-                <div className='y-axis-item'>
-                    {i}k
-                </div>
-            )}
-        </div>
-        <div className='grid-content'>
-        <div className='grid'>
-            {[...Array(121)].map((_, i) => 
-                <div className='grid-line'/>
-            )}
-        </div>
-        <div className='grid-x-axis'>
-            {[...Array(11)].map((_, i) => 
-                {
+        <div className='grid-component'>
+            <div className='gold-earned-headline'>
+                Gold Earned
+            </div>
+            <div className='grid-container'>
+            <div className='grid-y-axis'>
+                {axisArray.map((i) => 
+                    <div className='y-axis-item'>
+                        {i}k
+                    </div>
+                )}
+            </div>
+            <div className='grid-content'>
+            <div className='grid'>
+                {grid && grid.map((_, sub) =>
+                    _.map((element, i) => 
+                    <div className='grid-line' style={{backgroundColor: element === 0 ? 'white' : '#B567D9', transitionDelay: `${2+(i/(sub+10))}s`}}/>
+                    ) 
+                )}
+            </div>
+            <div className='grid-x-axis'>
+                {[...Array(11)].map((_, i) => 
+                    {
 
-                    return (
-                        <div className='x-axis-item'>
-                        {(i+1) % 2 == 0 && (i+1) / 2}
-                        </div>
-                    )
-                }
-            )}
-        </div>
-        </div>
+                        return (
+                            <div className='x-axis-item'>
+                            {(i+1) % 2 == 0 && (i+1) / 2}
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+            <div className='x-axis-label'>
+                Games
+            </div>
+            </div>
+            </div>
         </div>
     )
 
