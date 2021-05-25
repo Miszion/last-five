0438000fbd16e961a3f5c7904cdb1fd2f890a139
Mission Marcus <missionmarcus@gmail.com>
2021-05-25 00:19:07 -0500

    added qol style changes

--- src/components/Loading/loading.jsx
+++ src/components/Loading/loading.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
  
 import './loading.css'
 
 const Loading = (props) => {
 
 
+    const [selected, setSelected] = useState(0)
+
+
+    useEffect(() => {
+
+        let interval = setInterval(() => {
+            selected < 2 ? setSelected(selected + 1) : setSelected(0)
+        }, 300)
+
+        return () => clearInterval(interval)
+
+    }, [selected])
+
+
     return (
-        <div className='loader'></div>
+        <div className='loader'>
+            <div className='loading-text'>
+                Loading Summoner
+            </div>
+            <div className='circle-grid'>
+                <div className={`loading-circle ${selected === 0 && 'active'}`}/>
+                <div className={`loading-circle ${selected === 1 && 'active'}`}/>
+                <div className={`loading-circle ${selected === 2 && 'active'}`}/>
+            </div>
+        </div>
     )
 
 }
